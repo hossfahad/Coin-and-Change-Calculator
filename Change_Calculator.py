bf35435897a0ade1474c5d7a6c0d5fb972bbdedf
Fahad Hossain <hoss.fahad@gmail.com>
2017-07-25 01:47:17 -0400

    Update Change_Calculator.py

--- Change_Calculator.py
+++ Change_Calculator.py
@@ -17,3 +17,20 @@ def(change)
 change = ("paid" - "cost")
 # This presents the change
 print("Your change is:" change)
+
+# This portion of the code will separate the change in quarters, dimes, nickels and pennies.
+def(change_whole)
+change_whole = raw_input("Please enter an amount of change")
+
+
+"quarters" = int(change_whole // 25) # Number of quarters
+change_whole -= "quarters" * 25  # Subtracts amount in quarters
+"dimes" = int(change_whole // 10) # Number of dimes
+change_whole -= "dimes" * 10 # Subtracts amount in dimes
+"nickels" = int(change_whole // 5) # Number of nickels
+change_whole -= "nickels" * 5 # Subtracts amount in nickels
+"pennies" = int(change_whole) # Number of pennies
+
+
+# This will present the change in USD($) coins
+print("Your change is in %d quarters, %d dimes, %d nickels and %d pennies") % ("quarters", "dimes", "nickels", "pennies")
