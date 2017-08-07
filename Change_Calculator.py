d0e8a5afdb7940282c956e25ead7d5182a6fbc5a
Fahad Hossain <hoss.fahad@gmail.com>
2017-08-07 16:55:56 -0400

    Update Change_Calculator.py

--- Change_Calculator.py
+++ Change_Calculator.py
@@ -16,18 +16,27 @@ change = (paid - cost)
 print("Your change is:", ("%.2f" % change))
 
 # This portion of the code will separate the change in quarters, dimes, nickels and pennies.
-def(change_whole)
-change_whole = raw_input("Please enter the amount of change:")
 
+# The value of coins in USD.
+quarter = 25
+dime = 10
+nickel = 5
+penny = 1
 
-"quarters" = int(change_whole // 25) # Number of quarters
-change_whole -= "quarters" * 25  # Subtracts amount in quarters
-"dimes" = int(change_whole // 10) # Number of dimes
-change_whole -= "dimes" * 10 # Subtracts amount in dimes
-"nickels" = int(change_whole // 5) # Number of nickels
-change_whole -= "nickels" * 5 # Subtracts amount in nickels
-"pennies" = int(change_whole) # Number of pennies
+# This shows how to retrieve the change that will be separated.
+paid = int(float(paid) * 100)
+cost = int(float(cost) * 100)
+change = paid - cost
 
+# This portion subtracts the total once divided by a quarter, which is then divided into dimes and so on.
+qb = change / quarter
+qpartialchange = change - qb * quarter
+db = qpartialchange // dime
+dpartialchange = qpartialchange - db * dime
+nb = dpartialchange // nickel
+npartialchange = dpartialchange - nb * nickel
+pb = npartialchange // penny
+ppartialchange = npartialchange - pb * penny
 
-# This will present the change in USD($) coins
-print("Your change is in %d quarters, %d dimes, %d nickels and %d pennies") % ("quarters", "dimes", "nickels", "pennies")
+# This prints the change in their respective coin.
+print("Your change is in %s quarters, %s dimes, %s nickels and %s pennies") % (qb, db, nb, pb)
